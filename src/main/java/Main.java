3e1fb071cd5a9d14671813073c7a0c928aa9d2f6
aknurason <aknurason@gmail.com>
2023-09-10 17:56:39 +0600

    change6

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args){
-        String messagess = "Hello, yyy qa.guru!";
+        String messagess = "Hello, yyyjjj qa.guru!";
         System.out.println(messagess);
     }
 }
