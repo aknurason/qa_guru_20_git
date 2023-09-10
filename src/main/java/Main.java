939f8d21235985e60433ab3ca3a6fe749fa33b4b
aknurason <aknurason@gmail.com>
2023-09-10 17:53:04 +0600

    change 4

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args){
-        String message = "Hello, qa.guru!";
-        System.out.println(message);
+        String messagess = "Hello, qa.guru!";
+        System.out.println(messagess);
     }
 }
