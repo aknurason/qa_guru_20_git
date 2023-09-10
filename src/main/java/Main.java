c6b3cc2cf3e63c852eb4db3219df29b17169fc3e
aknurason <aknurason@gmail.com>
2023-09-10 18:06:10 +0600

    change7

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 public class Main {
-    public static void main(String[] args){
-        String messagess = "Hello,  qa.guru!";
-        System.out.println(messagess);
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Hello,  qa.guru!");
+        }
     }
-}
+    }
